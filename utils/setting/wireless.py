fabe02fa74d09feb77ef0ef30f3a6880b576f50f
thangg_trann_12 <48148298+thanggtrann12@users.noreply.github.com>
2022-04-28 16:22:57 +0700

    add msg pop up

--- utils/setting/wireless.py
+++ utils/setting/wireless.py
@@ -2,11 +2,7 @@ import os
 def check_ping():
     hostname = "google.com"
     response = os.system("ping  " + hostname)
-    # and then check the response...
     if response == 0:
         return True
     else:
         return False
-
-
-print(check_ping())
